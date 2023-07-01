176ed484abe5cf7726902408a7191396ea9bb95d
Alexandre Retamero <alexandreretamero@gmail.com>
2023-07-01 13:20:33 -0300

    content contact

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,5 +1,18 @@
 import styles from './styles.module.scss';
 
 export const Contact = () => {
-  return <section>Contact</section>;
+  return (
+    <section>
+      <h2>Fale com a gente</h2>
+      <span>Responderemos o seu contato o mais breve possível.</span>
+      <form>
+        <input type="text" placeholder="Nome" />
+        <input type="text" placeholder="Nome" />
+        <input type="text" placeholder="Nome" />
+        <input type="text" placeholder="Nome" />
+        <textarea placeholder="Mensagem" />
+        <button className='button'>ENVIAR MENSAGEM</button>
+      </form>
+    </section>
+  );
 };
